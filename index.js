735742ae97e2618fd28b3b4e34a49f8fe42f9f21
4zz268 <owen.akelo@student.moringaschool.com>
2025-03-17 11:28:09 +0300

    add index.html

---
+++ index.js
@@ -0,0 +1,18 @@
+const ramens = [
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+
+ ];
+ document.addEventListener('DOMContentLoaded', () => {
+    const ramenmenu =document .getElementById('ramen-menu');
+    const detailimage = document.getElementById('detail-image');
+    const ratingDisplay = document.getElementById('rating-display');
+    const commentDisplay = document.getElementById('comment-display');
+    const detailname = document.getElementById('detail-name');
+    const newRamenform = document.getElementById('new-ramen');
+
+
+
+    const ramendata= ramens{
+'gyukotsu':{ rating : '9/10', comment: '
